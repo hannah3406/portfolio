45c8331d15a2494da4d78f8806486b340c03856b
hannah3406 <86402077+hannah3406@users.noreply.github.com>
2021-10-11 00:31:55 +0900

    20211011

--- admin/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $host = "localhost";
-    $userId = "root";
-    $userPw = "test1234";
-    $DateBase = "project";
-    $connect = mysqli_connect($host,$userId,$userPw,$DateBase);
-
-?>
